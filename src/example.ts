353d36385578c746285799556c741bffaed4cc27
wisdom ekpotu <whizfidel@gmail.com>
2022-09-29 12:05:32 +0100

    added examples

---
+++ src/example.ts
@@ -0,0 +1,22 @@
+// interface Iitem {
+//   text: string
+// }
+
+//declaring component 0
+//props have type of interface
+// const Header = (props: Iitem) => {
+//  console.log(props.text)
+//};
+
+// declaring component 1
+//destructure the prop
+// const Header = ({title}: Iitem) => {
+// console.log(text)
+// };
+
+//declaring component 1
+// const Header: FC<Props> = ({ title }) => {};
+
+// import AddItem, {
+//   IItem,
+// } from './src/components/AddItem'; /* import AddItem and interface*/
